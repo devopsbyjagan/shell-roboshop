d0bf6f7101382630d15a418dff1a6078f36db3fb
devopsbyjagan <jaganazure9@gmail.com>
2026-09-01 21:41:39 +0530

    mongo script

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -4,7 +4,7 @@ LOGS_FOLDER="/var/log/roboshop"
 sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
-LOGS_FILE="$LOG_FOLDER/$0.log"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
 
 USERID=$(id -u)
 R="\e[31m"
@@ -13,19 +13,19 @@ Y="\e[33m"
 N="\e[0m"
 TIMESTAMP=$(date "+%y-%m-%d %H:%M:%S")
 
-if [USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
     echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
     exit 1
 f1
 
-VALIDATE()
-    if [$1 -ne 0 ]; then
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
         echo -e "$TIMESTAMP [ERROR] $2 ..... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e "$TIMESTAMP [INFO] $2.... $G SUCCESS $N |  tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [INFO] $2.... $G SUCCESS $N" |  tee -a $LOGS_FILE
     fi
-
+}
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding mongo repo"
 
@@ -38,5 +38,5 @@ VALIDATE $? "starting and enabling mongodb"
 sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
 VALIDATE $? "Allowing remote connections to MongoDB"
 
-sytemctl restart mongod
+systemctl restart mongod
 VALIDATE $? "Restarting MongoDB"
